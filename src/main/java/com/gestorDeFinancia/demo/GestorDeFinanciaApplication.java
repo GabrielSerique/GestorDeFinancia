ed2cb38812ae26e9aaa86708b26c694d5ac6b33c
GabrielSerique <enzogabriel1214@gmail.com>
2025-05-06 08:48:33 -0300

    inicio do projeto

---
+++ src/main/java/com/gestorDeFinancia/demo/GestorDeFinanciaApplication.java
@@ -0,0 +1,13 @@
+package com.gestorDeFinancia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeFinanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeFinanciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestorDeFinancia/demo/GestorDeFinanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestorDeFinancia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeFinanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
